02f901d364baff9aca20726d2a85e2aeeb2c7503
Alex <wongalex2@gmail.com>
2025-06-21 13:37:03 -0700

    init commit

---
+++ src/test/java/habit/tracker/backend/HabitTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package habit.tracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
